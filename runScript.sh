e88334e880d5f274268b0383d0d199f3916d2539
arunkumarshahi@gmail.com <arunkumarshahi@gmail.com>
2020-09-29 23:25:37 +0530

    fix:api gateway mapping

--- runScript.sh
+++ runScript.sh
@@ -1,10 +1,10 @@
 cd api-gateway
-sudo ./mvnw spring-boot:run
+sudo mvn spring-boot:run
 cd ../gamification
-sudo ./mvnw spring-boot:run
+sudo mvn spring-boot:run
 cd ../social-multiplication
-sudo ./mvnw spring-boot:run
+sudo mvn spring-boot:run
 cd ../ui
 sudo java -jar /opt/jetty9/start.jar
 cd ../service-registry
-sudo ./mvnw spring-boot:run
+sudo mvn spring-boot:run
