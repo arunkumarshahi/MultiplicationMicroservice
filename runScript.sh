1599f0ba1e5bd687c3744a555abe7fb21bef0046
arun k <kumararun@virtusa.com>
2020-09-29 13:58:20 +0530

    added api gateway mapping

---
+++ runScript.sh
@@ -0,0 +1,8 @@
+cd api-gateway
+sudo ./mvnw spring-boot:run
+cd ../gamification
+sudo ./mvnw spring-boot:run
+cd ../social-multiplication
+sudo ./mvnw spring-boot:run
+cd ../ui
+sudo java -jar /opt/jetty9/start.jar
