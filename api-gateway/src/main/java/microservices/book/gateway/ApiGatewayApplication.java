d6ce6da502fd61b254285b4ef131bbc7a69b0756
arun k <kumararun@virtusa.com>
2020-10-01 00:29:03 +0530

    fixing api gateway

--- api-gateway/src/main/java/microservices/book/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/microservices/book/gateway/ApiGatewayApplication.java
@@ -3,13 +3,17 @@ package microservices.book.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
+import microservices.book.gateway.config.RibbonConfiguration;
+
 @SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
-
+// test to check commit 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
